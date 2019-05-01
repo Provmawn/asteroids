fdeb17ba9ab8036cfe49a41b40bf7e1d34f4605b
amman sandhu <provmawn@gmail.com>
2019-04-30 20:36:49 -0700

    Fix merge errors

--- source/rock.cpp
+++ source/rock.cpp
@@ -3,27 +3,22 @@
 extern const int SCREEN_WIDTH;
 extern const int SCREEN_HEIGHT;
 
-
-
 Rock::Rock() {
     hitbox.x=0;
     hitbox.y=0;
     hitbox.w=10;
     hitbox.h=10;
-<<<<<<< HEAD:source/rocks.cpp
     posX = 0;
     posY = 0;
     velX = 1;
     velY = 1;
     subX = 0;
     subY = 0;
-=======
     posX = ::SCREEN_WIDTH / 2;
     posY = ::SCREEN_HEIGHT / 2;
     std::cout << __FILE__ << ": constructor..." << std::endl;
     velX = (rand() % 10) + 1;
     velY = (rand() % 10) + 1;
->>>>>>> ce85b607010d22b2176ba3edfaabb65e9e5ec8a4:source/rock.cpp
 }
 
 Rock::~Rock() {
@@ -42,8 +37,7 @@ void Rock::update() {
     }
 }
 
-<<<<<<< HEAD:source/rocks.cpp
-void Rocks::subpixel() {
+void Rock::subpixel() {
 	//assume signs of movement are positive
 	    int signX = copysign(1.0,velX);
 	    int signY = copysign(1.0,velY);
@@ -63,10 +57,7 @@ void Rocks::subpixel() {
 
 
 
-void Rocks::render(SDL_Renderer* render) {
-=======
 void Rock::render(SDL_Renderer* render) {
->>>>>>> ce85b607010d22b2176ba3edfaabb65e9e5ec8a4:source/rock.cpp
     SDL_SetRenderDrawColor(render, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderFillRect(render, &hitbox);
 }
